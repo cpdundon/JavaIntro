0a5490ea2af82d9fce43b1441df96927f3cd4c66
Chris Dundon <chris.dundon@gmail.com>
2021-01-31 00:53:14 -0500

    have worked out the code structure.  need to spend time on formatting and make the calcs work

--- app/src/main/java/com/example/javaintro/MainActivity.java
+++ app/src/main/java/com/example/javaintro/MainActivity.java
@@ -4,142 +4,93 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
     private MaterialTextView tvDisplay;
-    private MaterialButton btnC;
-    private MaterialButton btnDel;
-    private MaterialButton btnPer;
-    private MaterialButton btn1;
-    private MaterialButton btn2;
-    private MaterialButton btn3;
-    private MaterialButton btn4;
-    private MaterialButton btn5;
-    private MaterialButton btn6;
-    private MaterialButton btn7;
-    private MaterialButton btn8;
-    private MaterialButton btn9;
-    private MaterialButton btn0;
-
-    private String strDisplay = "0";
+    private double priorValue = 0.0;
+    private String[] operatorIds = new String[] {Integer.toString(R.id.btn_equals), Integer.toString(R.id.btn_opr_div),
+            Integer.toString (R.id.btn_opr_x), Integer.toString(R.id.btn_opr_minus), Integer.toString(R.id.btn_opr_plus)};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calculator);
+        LinearLayout llTop = findViewById(R.id.ll_top_parent);
+
         tvDisplay = findViewById(R.id.tv_display);
-        btnC = findViewById(R.id.btn_c);
-        btnDel = findViewById(R.id.btn_del);
-        btnPer = findViewById(R.id.btn_per);
-        btn1 = findViewById(R.id.btn_1);
-        btn2 = findViewById(R.id.btn_2);
-        btn3 = findViewById(R.id.btn_3);
-        btn4 = findViewById(R.id.btn_4);
-        btn5 = findViewById(R.id.btn_5);
-        btn6 = findViewById(R.id.btn_6);
-        btn7 = findViewById(R.id.btn_7);
-        btn8 = findViewById(R.id.btn_8);
-        btn9 = findViewById(R.id.btn_9);
-        btn0 = findViewById(R.id.btn_0);
-
-        btnC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //intCount++;
-                tvDisplay.setText("0");
+        for(int i = 0; i < llTop.getChildCount(); i++)
+        {
+            if(llTop.getChildAt(i) instanceof LinearLayout)
+            {
+                processButtons((LinearLayout) llTop.getChildAt(i));
             }
-        });
+        }
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+    }
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+    private void processButtons(LinearLayout ll) {
+        for(int i = 0; i < ll.getChildCount(); i++)
+        {
+            if(ll.getChildAt(i) instanceof MaterialButton)
+            {
+                MaterialButton mB = (MaterialButton) ll.getChildAt(i);
 
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+                if(mB.getId() == R.id.btn_clr) {
+                    mB.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            tvDisplay.setText("");
+                        }
+                    });
 
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+                    continue;
+                }
 
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+                if(mB.getId() == R.id.btn_del) {
+                    mB.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String strCurrDisp = tvDisplay.getText().toString();
+                            tvDisplay.setText(strCurrDisp.substring(0,strCurrDisp.length()-1));
+                        }
+                    });
 
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+                    continue;
+                }
 
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+                if(Arrays.asList(operatorIds).contains(Integer.toString(mB.getId()))) {
+                    mB.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            processOperator((MaterialButton) v);
+                        }
+                    });
 
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
+                    continue;
+                }
 
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
+                mB.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        updateView((MaterialButton) v);
+                    }
+                });
             }
-        });
-
-        btn0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
-
-        btnPer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateView((MaterialButton) v);
-            }
-        });
-
-        btnDel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String strCurrDisp = tvDisplay.getText().toString();
-                tvDisplay.setText(strCurrDisp.substring(0,strCurrDisp.length()-1));
-            }
-        });
+        }
     }
 
     private void updateView(MaterialButton btn) {
-        tvDisplay.setText(tvDisplay.getText().toString() + btn.getText().toString());
+        tvDisplay.append(btn.getText());
+    }
+
+    private void processOperator(MaterialButton btn) {
+        tvDisplay.setText("Start Operation");
     }
 }
